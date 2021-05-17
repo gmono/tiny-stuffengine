c7fe1337fbb30ce4a7b7e7cff051498a3bcab25c
gmono <gaozijian0411@foxmail.com>
2021-05-17 18:09:35 +0800

    编写完成添加重力 尚未测试

--- src/components/GravityComponent.ts
+++ src/components/GravityComponent.ts
@@ -12,11 +12,12 @@
 
  import { ComponentBase } from "../Component";
  import { List, TimeSpan } from "../Common";
- import { Stuff } from "../Stuff";
- import { Rank, Tensor, Tensor1D } from "@tensorflow/tfjs-core";
+ import { Stuff, StuffOperation } from "../Stuff";
+ import { Rank, Tensor, Tensor1D, zeros } from "@tensorflow/tfjs-core";
  import { assert } from "ts-pystyle";
  import { MechanicsComponent } from "./MechanicsComponent";
 import { RenderPropsComponent } from "./RenderPropsComponent";
+import { tensor } from "@tensorflow/tfjs";
  //需要有一个“shape”的类来表示具体的形状
  //如通过顶点定义 shape=set of points
  //或通过html content来定义 一个shape就是一个 element
@@ -34,13 +35,16 @@ import { RenderPropsComponent } from "./RenderPropsComponent";
      //质量 这个由力学组件提供
      name: "gravity" = "gravity";
 
+     //临时变量 每次render清除
+     nowUseGravity=zeros([3])
      exports = {
          /**
-          * 施加一个重力的力
+          * 施加一个重力的力 这个力会累积 并在下次渲染的前被清除
           * @param g 施加一个重力
           */
          useGravity:(g:Tensor1D)=>{
-             
+             assert(g.shape[0]==3)
+             this.nowUseGravity=this.nowUseGravity.add(g)
          }
      };
      beforeAttach(stuff: Stuff) {
@@ -60,7 +64,29 @@ import { RenderPropsComponent } from "./RenderPropsComponent";
              let shape=this.Stuff.getComponent<RenderPropsComponent>("render")
              let center=shape.getPosition()
              let G=6.67e-11
-             let F=G*(m*M)/(r*r)
+             for(let s of gstuff){
+                 //s是目标物体 计算到目标物体的力
+                 //计算从物体s到本物体的向量
+                 let opera=new StuffOperation(s);
+                 let gr=opera.getComponent<GravityComponent>("gravity")
+                 let srender=opera.getComponent<RenderPropsComponent>("render");
+                 let smech=opera.getComponent<MechanicsComponent>("mechanics")
+                 //计算r
+                 let spos=srender.getPosition() as Tensor1D;
+                 //这是从目标指向本物体的向量
+                 let bta=spos.sub(center);
+                 let bta_norm=bta.div(bta.norm())
+                 let r=bta.norm();
+                 //目标质量
+                 let M=smech.getM()
+                 //计算引力大小
+                 let F=tensor(G*(m*M)).div((r.pow(2)))
+                 //乘单位
+                 let FP=F.mul(bta_norm) as Tensor1D;
+                 assert(FP.rank==1,"重力维度错误")
+                 gr.useGravity(FP)
+             }
+             
              //开始计算重力 公式 
          }
      }
